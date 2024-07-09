bf4126d0ad0ce4f2b89531b52a2d5d23a3a9f1c6
abraham <abrahammathewos.meja.5695@student.uu.se>
2024-07-08 23:52:01 -0500

    Autorization in progress

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+    providers: [
+        {
+            domain: "https://finer-chow-44.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
